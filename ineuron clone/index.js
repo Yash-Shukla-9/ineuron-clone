53ce5b3cfbb3b32e811d82346ad5c329b88446cc
Yash <60171382+Yash-Shukla-9@users.noreply.github.com>
2023-06-06 14:21:37 +0530

    Add files via upload

---
+++ ineuron clone/index.js
@@ -0,0 +1,10 @@
+import * as bootstrap from "bootstrap";
+
+import Scrollbar from "smooth-scrollbar";
+
+const options = {
+  damping: 0.06,
+  alwaysShowTracks: true,
+};
+
+Scrollbar.init(document.querySelector("#my-scroll"), options);
